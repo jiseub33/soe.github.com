16495b92fc3179f00cd81361d6727b1a6521e174
jiseub33 <jiseub33@gmail.com>
2024-06-11 22:56:21 +0900

    update

---
+++ js/alert.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const content4 = document.querySelector('.content4');
+    if (content4) {
+        const links = content4.querySelectorAll('a');
+        links.forEach(link => {
+            link.addEventListener('click', (event) => {
+                event.preventDefault(); // Prevent the default link behavior
+                alert('준비중입니다!');
+            });
+        });
+    }
+});
