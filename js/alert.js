09e41d1f3776820232c677f469fe3a9f325aa661
jiseub33 <jiseub33@gmail.com>
2024-06-18 14:13:01 +0900

    update

--- js/alert.js
+++ js/alert.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const content4 = document.querySelector('.content4');
     if (content4) {
-        const links = content4.querySelectorAll('a');
+        const links = content4.querySelectorAll('.store');
         links.forEach(link => {
             link.addEventListener('click', (event) => {
                 event.preventDefault(); // Prevent the default link behavior
